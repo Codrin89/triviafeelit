92b12a928cc57110209fe9d62392ad3fd3bec0af
Codrin89 <cogalnicianu.codrin89@gmail.com>
2019-10-25 22:07:59 -0700

    routed

--- index.js
+++ index.js
@@ -60,11 +60,11 @@ app.get('/test', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/templates/view.html'));
+	res.sendFile(path.join(__dirname, '/public/view.html'));
 });
 
 app.get('/list', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/templates/view.html'));
+	res.sendFile(path.join(__dirname, '/public/view.html'));
 });
 
 app.get('/upload', function (req, res) {
